35a6640d9c1367bea746fb8798887d5036c105c9
Aveon Gilmore <gilmoave000@warren.k12.in.us>
2025-07-29 13:51:22 -0400

    updates

--- src/HelloJAVA.java
+++ src/HelloJAVA.java
@@ -3,6 +3,13 @@ public class HelloJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Java");
 
+        String name = "aveon";
+        String age = "17";
+        System.out.print("My name is: ");
+        System.out.println(name);
+        System.out.print("My age is:");
+        System.out.println(age);
+
 
 
 
